f0e032999118e037f827c06aff9cb2a838755cdd
entitybtw <studenov.ivan020312@gmail.com>
2026-01-03 17:26:40 +0300

    add wav to build.sh. add error_original.wav, error.wav. comment error wav check in lua.c

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 rm -f libs/include_res/output_png.c 
 bin2c res/error.png libs/include_res/output_png.c error_data 
+rm -f libs/include_res/output_wav.c 
+bin2c res/error.wav libs/include_res/output_wav.c system_sound 
 make clean && make
